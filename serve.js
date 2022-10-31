5bad56f81abd3248413356e5e920b196ceec6638
Coelacanth101 <106444731+Coelacanth101@users.noreply.github.com>
2022-10-31 16:10:55 +0900

    'commit'

--- serve.js
+++ serve.js
@@ -2198,7 +2198,7 @@ lastActionPlayer:'',allResource:{ore:0,grain:0,wool:0,lumber:0,brick:0},
     const logdata = {action:'takeover', playername:this.players[player.number].name, socketID:player.socketID}
     setTimeout(()=>{
       display.message(logdata)
-    },1000)
+    },2000)
   },
   initialize(){
     this.players.length = 0;
@@ -3305,7 +3305,7 @@ io.on("connection", (socket)=>{
   if(game.phase !== 'nameinputting'){
     setTimeout(()=>{
       io.to(socket.id).emit("pleasetakeover",'')
-    },1000)
+    },2000)
   }
   
   //名前の入力
